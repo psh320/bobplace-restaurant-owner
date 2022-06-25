8b677d2c788087c4ee59ae4ceebd1f7cdc4981ff
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-06-25 16:42:35 +0900

    Install react-native-navigation 관련 라이브러리

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  ],
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
 };
