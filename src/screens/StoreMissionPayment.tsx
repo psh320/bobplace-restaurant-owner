fba6a386d8fc6a48e0a1cfec8accae118b8251a3
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-07-04 01:10:28 +0900

    Feat: add payment page

--- src/screens/StoreMissionPayment.tsx
+++ src/screens/StoreMissionPayment.tsx
@@ -13,7 +13,12 @@ import {MissionDetailCard} from '../components/mission/MissionDetailCard';
 
 type Props = StackScreenProps<StoreStackParamList, 'StoreMissionPayment'>;
 
-const dummyPurchase = {userName: '박승민', purchaseDate: '2022.02.02 월요일 12:07:13', point: 500};
+const dummyPurchase = {
+  userName: '박승민',
+  purchaseDate: '2022.02.02 월요일 12:07:13',
+  point: 500,
+  purchaseId: 1223,
+};
 
 const StoreMissionPayment = ({navigation, route}: Props) => {
   const insets = useSafeAreaInsets();
@@ -32,6 +37,44 @@ const StoreMissionPayment = ({navigation, route}: Props) => {
           </TouchableOpacity>
           <Text style={[styles.screenHeaderTitle]}>결제 취소 요청</Text>
           <Icon name="arrow-left" size={24} color="black" style={{opacity: 0}} />
+        </View>
+        <View style={[styles.missionCard]}>
+          <View style={[styles.cancelWrap]}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('StoreMissionPayment', {purchaseId: purchaseId});
+              }}
+            >
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginBottom: 8,
+                  width: '100%',
+                }}
+              >
+                <Text style={[styles.normalText]}>상세정보</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.seperateLine]} />
+
+          <View style={[styles.infoRow]}>
+            <Text style={[styles.fieldText]}>고객명</Text>
+            <Text style={[styles.normalText]}>{dummyPurchase.userName}</Text>
+          </View>
+          <View style={[styles.infoRow]}>
+            <Text style={[styles.fieldText]}>결제일</Text>
+            <Text style={[styles.normalText]}>{dummyPurchase.purchaseDate}</Text>
+          </View>
+          <View style={[styles.infoRow]}>
+            <Text style={[styles.fieldText]}>포인트</Text>
+            <Text style={[styles.normalText]}>{dummyPurchase.point}P</Text>
+          </View>
+          <View style={[styles.infoRow]}>
+            <Text style={[styles.fieldText]}>구분번호</Text>
+            <Text style={[styles.normalText]}>{dummyPurchase.purchaseId}</Text>
+          </View>
         </View>
       </View>
     </>
@@ -81,4 +124,50 @@ const styles = StyleSheet.create({
     fontFamily: 'Pretendard-SemiBold',
     color: '#C8C8C8',
   },
+
+  missionCard: {
+    backgroundColor: '#FFFFFF',
+    borderColor: '#EFEFEF',
+    borderWidth: 1,
+    padding: 16,
+  },
+  cancelWrap: {justifyContent: 'center', alignItems: 'flex-start'},
+  missionMain: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  seperateLine: {
+    borderWidth: 0.5,
+    width: '100%',
+    borderColor: '#DFDFDF',
+    marginBottom: 12,
+  },
+  nameText: {
+    fontFamily: 'Pretendard-Medium',
+    color: '#111111',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  fieldText: {
+    fontFamily: 'Pretendard-Light',
+    color: '#616161',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  cancelText: {
+    fontSize: 14,
+    lineHeight: 22,
+    fontFamily: 'Pretendard-Light',
+    color: '#949494',
+  },
+  infoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 4,
+    alignItems: 'center',
+  },
+
+  normalText: {fontFamily: 'Pretendard-Medium', color: '#111111', fontSize: 16, lineHeight: 24},
 });
