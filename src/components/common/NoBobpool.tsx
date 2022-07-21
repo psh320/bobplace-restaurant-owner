539bdeeab867d7103976035b876f34778c294e98
yejinleee <yejin9487@gmail.com>
2022-07-21 13:54:21 +0900

    Feat: 미션화면 밥풀이

--- src/components/common/NoBobpool.tsx
+++ src/components/common/NoBobpool.tsx
@@ -11,9 +11,9 @@ export const NoBobpool: FC<NoBobpoolProps> = ({category}) => {
     <View style={[DesignSystem.centerArrange, {flex: 1}]}>
       <Text style={[DesignSystem.title1SB, {color: '#111111', marginBottom: 12}]}>
         {category === '진행중'
-          ? '진행중 없음'
+          ? '미션중인 유저가 없어요'
           : category === '성공요청'
-          ? '성공요청 없음'
+          ? '성공 요청이 없어요'
           : category === '미션상세'
           ? '결제한 미션들 없음'
           : category === '리뷰'
